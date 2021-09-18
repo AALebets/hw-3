455fb44dcd6f8c96b650d667e8c7e913f9315f63
AALebets <89778658+AALebets@users.noreply.github.com>
2021-09-18 22:12:09 +0300

    Update ATM.java

--- ATM.java
+++ ATM.java
@@ -18,8 +18,8 @@ public int countBanknotes(int sum){
     }
        
         public static void main(String[] args) {
-        ATM test = new ATM();
+        ATM atm = new ATM();
         // should be 8 (500 + 200 + 50 + 20 + 10 + 5 + 2 + 2);
-        System.out.println(test.countBanknotes(789));
+        System.out.println(atm.countBanknotes(789));
     }
 }
